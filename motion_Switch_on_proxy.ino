9293035ca4339d5bf1536413cea1192aa5265f17
Sushant Sangle <sangle_sushant@yahoo.in>
2019-03-17 10:20:56 +0530

    First full initial task done,upto mountain--not rebased yet

--- motion_Switch_on_proxy.ino
+++ motion_Switch_on_proxy.ino
@@ -5,10 +5,21 @@ void proxy()
   if(p0==0 && !pMode1)
   {
     staticG();
+    Hm[0]=1.0;
+    Hm[1]=1.0;
+    left();
     getangle(4);
     pMode1++;
   }
-  if(p1==0 &&  !pMode) pMode++ ;
+  if(p1==0 &&  !pMode)
+  {
+//    staticG();
+//    left();
+//    getangle(4);
+    Hm[1]=1.5;
+    Hm[0]=1.5;
+    pMode++ ;
+  }
   if(p1==1 && pMode==1 && (els[0][1]==-100 && els[1][1]==-100))  pMode++;  
   
   if((i-46)%90==0)
@@ -17,14 +28,12 @@ void proxy()
     if(p1==1 &&  pMode>=1 && (els[0][1]==-100 && els[1][1]==-100) && (LEG==0 || LEG==1)) pMode++ ;
     
     if(LEG==0 && els[0][1]==0 && pMode==1){
-      straight();
-      els[0][1]=-100;
+      els[0][1]= -100;
       Hm[0]=0.8f;
       getangle(0);
     }
     if(LEG==1 && els[1][1]==0 && pMode==1){
-      straight();
-      els[1][1]=-100;
+      els[1][1]= -100;
       Hm[1]=0.8f;
       getangle(1);
     }
@@ -41,23 +50,23 @@ void proxy()
     }
     if(els[1][1]==0 && els[0][1]==0 && pMode==10 && S==7000){
       alternate();
-      straight();
+      left();
       startC=1;
       getangle(4);
     }
   }
-  if(steps==10)
+  if(steps==21)
   {
     rightEx();
     getangle(4);
     steps=100;
   }
-  if(steps==115)
+  if(steps==122)
   {
     straight();
     staticG();
     getangle(4);
-    steps==200;
+    steps=200;
   }
 }
 
